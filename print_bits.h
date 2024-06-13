6297d9b3f5b062b8a6fe3829d6c3bfa13efeb63f
re7adotcom <149816497+re7adotcom@users.noreply.github.com>
2024-06-13 23:45:17 +0200

    Update print_bits.h

--- print_bits.h
+++ print_bits.h
@@ -1,19 +1,17 @@
+// print_bits.h
+// (c) Reza Manoochehrian JUN-2024
+
 #include <fstream>
 #include <vector>
 #include <string>
 
-enum class PrintMode
-{
-  bits
-  , bytes_raw
-  , bytes_pretty_binary
-};
+enum class PrintMode { bits, bytes_raw, bytes_pretty_binary };
 
 void print_bits
 (
-  PrintMode m
-  , std::ostream& o
-  , const std::vector<unsigned char>& v
+  PrintMode
+  , std::ostream&
+  , const std::vector<unsigned char>&
   , const unsigned long skip_bytes // 0: no skipping, N: skip first N bytes
   , const unsigned long read_bytes // 0: read all, N: read up to N bytes
   , const unsigned long line_break // 0: no line break, N: line break every N bytes
